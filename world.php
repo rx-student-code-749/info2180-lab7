42ffaed617f372b9a4ad26a669df13acf865cd5c
R <rapheekwon@gmail.com>
2018-11-13 14:10:11 -0500

    Completed exercise 3.

--- world.php
+++ world.php
@@ -11,12 +11,30 @@ if (!array_key_exists('C9_USER', $_ENV)) {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 }
 
-$stmt = $conn->query("SELECT * FROM countries");
+if (!array_key_exists('q', $_GET)) {
+    $stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $results = $stmt->fetchAll(\PDO::FETCH_ASSOC);
 
-echo '<ul>';
-foreach ($results as $row) {
-  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    $html =  '<ul>';
+    foreach ($results as $row)
+        $html .= '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    $html .= '</ul>';
+
+    echo $html;
+} else {
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%{$_GET['q']}%'");
+//    $_GET['q'] = ucwords($_GET['q']);
+
+    $results = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+    $html =  '<ul>';
+    foreach ($results as $row) {
+        $tmp = "<li>{$row['name']} is ruled by {$row['head_of_state']}</li>";
+        $html .= preg_replace("/\w*?".preg_quote($_GET['q'])."\w*/i", "<strong>$0</strong>", $tmp);
+//        $html .= str_ireplace($_GET['q'], "<strong>{$_GET['q']}</strong>", $tmp);
+    }
+//        $html .= '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    $html .= '</ul>';
+    echo $html;
 }
-echo '</ul>';
